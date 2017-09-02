065918703983f665fcb36f9b0e525f9f377cd752
rajukbn <31545081+rajukbn@users.noreply.github.com>
2017-09-02 21:40:08 +0530

    Update AppTest.java

--- src/test/java/com/codebind/AppTest.java
+++ src/test/java/com/codebind/AppTest.java
@@ -3,6 +3,10 @@ package com.codebind;
 import static org.junit.Assert.*;
 
 public class AppTest {
+    public void stringtest() throws Exception {
+        App ap=new App();
+        assertEquals("Welcom to mavendemo",ap.stringtest());
+    }
     @org.junit.Test
     public void test1() throws Exception {
         App ap=new App();
