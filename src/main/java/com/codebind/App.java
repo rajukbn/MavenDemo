f7dfbaeff53b299a0a81d1366cc43898f44c8b77
rajukbn <31545081+rajukbn@users.noreply.github.com>
2017-09-02 23:15:40 +0530

    Update App.java

--- src/main/java/com/codebind/App.java
+++ src/main/java/com/codebind/App.java
@@ -2,7 +2,7 @@ package com.codebind;
 
 public class App {
   public String test(){
-    return "Welcom to maven demo";
+    return "Welcom to maven Demo";
   }
   public String stringtest(){
     return "Welcom to maven demo1";
